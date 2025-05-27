2fb43e11438c323895d31cf797f0b1f64c09dd82
katiii-git <katymartagon@gmail.com>
2025-05-27 09:24:09 -0600

    Añadiendo view de home page

---
+++ src/app/pages/search-page/search-page.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-page.page.html',
+  styleUrl: './search-page.page.css'
+})
+export class SearchPagePage {
+
+}
